fbe8ac72510dd841985e117037bb36de0a0fb2ec
David Fredman <1152707+davfre@users.noreply.github.com>
2026-05-15 18:38:22 +0100

    chore(main): release oikontrol 2.14.0

--- modules/common/src/main/java/com/oikoaudio/common/OikontrolVersion.java
+++ modules/common/src/main/java/com/oikoaudio/common/OikontrolVersion.java
@@ -1,7 +1,7 @@
 package com.oikoaudio.common;
 
 public final class OikontrolVersion {
-    public static final String VERSION = "2.13.2"; // x-release-please-version
+    public static final String VERSION = "2.14.0"; // x-release-please-version
 
     private OikontrolVersion() {
     }
